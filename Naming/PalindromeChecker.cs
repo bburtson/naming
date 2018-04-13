92dc022ca36cc7f7e309424a0720632f30726320
Brett Burtson <brett@burtson.com>
2018-04-13 09:21:03 -0500

    Initial commit

---
+++ Naming/PalindromeChecker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleran.CodeTherapy.Naming.PalendromChecker
+{
+    public class PalindromeChecker
+    {
+
+        // pal·in·drome ˈpalənˌdrōm
+        // noun
+        // a word, phrase, or sequence that reads the same backward as forward, e.g., madam or nurses run.
+
+        public bool Test(string strInput)
+        {
+            string strTrimmed = strInput.Replace(" ", "");
+            string strReversed = new string(strTrimmed.Reverse().ToArray());
+            return strReversed.Equals(strReversed);
+        }
+
+        public bool Check(string input)
+        {
+            input = input.Replace(" ", "");
+            var reversed = new string(input.Reverse().ToArray());
+            return reversed.Equals(input);
+        }
+
+        public bool IsPalendrome(string input)
+        {
+            var forwards = input.Replace(" ", "");
+            var backwards = new string(forwards.Reverse().ToArray());
+            return backwards.Equals(forwards);
+        }
+    }
+}
---
+++ Naming/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soleran.CodeTherapy.Naming.PalendromChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrange
+            var checker = new PalindromeChecker();
+
+            var input = string.Empty;
+
+            // Act
+            while (!IsCommand(input))
+            {
+                input = PromptUserInput();
+                
+                var result = checker.Check(input);
+
+                WriteResult(input, result);
+            }
+
+        }
+
+        public static string PromptUserInput()
+        {
+            Console.WriteLine("Enter a word to see if its a palendrome -- or type quit to quit");
+
+            return Console.ReadLine();
+        }
+
+        public static void WriteResult(string input, bool result)
+        {
+            var output = IsCommand(input) ? "Goodbye! \n" : $"Palendrome: {result} \n";
+
+            Console.WriteLine(output);
+        }
+
+        public static bool IsCommand(string input) => input.Equals("quit", StringComparison.OrdinalIgnoreCase);
+
+
+    }
+}
